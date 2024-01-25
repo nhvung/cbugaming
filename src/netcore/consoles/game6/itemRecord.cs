3e1531d526764ae5b0575310c8fef4dca91612a7
vung <nhvung@live.com>
2024-01-25 19:44:56 +0700

    update code

--- src/netcore/consoles/game6/itemRecord.cs
+++ src/netcore/consoles/game6/itemRecord.cs
@@ -8,5 +8,11 @@ namespace game6
         public DateTime Time { get { return _Time; } set { _Time = value; } }
         string _Path;
         public string Path { get { return _Path; } set { _Path = value; } }
+        string[] _SplitPathNames;
+        public string[] SplitPathNames { get { return _SplitPathNames; } set { _SplitPathNames = value; } }
+        public override string ToString()
+        {
+            return $"{_Path}: {_Time:MM/dd/yyyy HH:mm:ss}";
+        }
     }
 }
